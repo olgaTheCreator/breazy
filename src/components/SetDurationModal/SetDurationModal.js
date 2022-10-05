25cdbcf2ac194215c97aca621da9854c087cc2a8
ojb <curylo.olga@gmail.com>
2022-10-05 13:54:35 +0100

    rc-slider put into work

--- src/components/SetDurationModal/SetDurationModal.js
+++ src/components/SetDurationModal/SetDurationModal.js
@@ -1,95 +1,65 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 import "../../styles/modalsStyle.css";
 import "./setDurationModal.css";
-import { Slider } from "../Slider/Slider";
-import {
-  durationToPosition,
-  positionToDuration,
-} from "../../utils/DurationAndPosition.js";
+import { DurationSlider } from "../Slider/Slider";
 
 export const SetDurationModal = ({
   setTimeOpen,
-  durationOfSession,
   setDuration,
   handleStop,
+  durationOfSession,
 }) => {
-  const ref = useRef(0);
-  const [sliderHeight, setSliderHeight] = useState(1);
-  const [isSliding, setSliding] = useState(false);
-  console.log(sliderHeight);
+  // const ref = useRef(0);
+  // const [sliderHeight, setSliderHeight] = useState(1);
+  // const [isSliding, setSliding] = useState(false);
+  // console.log(sliderHeight);
   // useEffect(() => {}, [sliderHeight]);
   // console.log(
   //   sliderTrackRef.current.clientHeight,
   //   durationToPosition(3, sliderTrackRef.current.clientHeight),
   //   durationOfSession
   // );
-  const [thumbPos, setThumbPos] = useState(0);
+  // const [thumbPos, setThumbPos] = useState(0);
   //   durationToPosition(durationOfSession, sliderHeight)
   // );
-  useEffect(
-    () =>
-      setThumbPos(
-        durationToPosition(
-          durationOfSession,
-          sliderHeight - sliderHeight * 0.068
-        )
-      ),
-    [sliderHeight]
-  );
-  console.log(thumbPos);
+  // useEffect(
+  //   () =>
+  //     setThumbPos(
+  //       durationToPosition(
+  //         durationOfSession,
+  //         sliderHeight - sliderHeight * 0.068
+  //       )
+  //     ),
+  //   [sliderHeight]
+  // );
+  // console.log(thumbPos);
+  let durdur;
+  const handleDurationChange = (value) => {
+    durdur = value;
+  };
   return (
     <div className="darkBG">
-      {/* <div className="modal-name">{""}</div> */}
-      {/* <div className="centered"> */}
-      <div className="time-modal" onMouseUp={() => setSliding(false)}>
+      <div className="time-modal">
         <div className="slider-parent">
-          {/* <div
-            className="buble-number"
-            style={{
-              top: `${thumbPos}px`,
-            }}
-          >
-            {positionToDuration(thumbPos, sliderHeight)}
-          </div> */}
-          <div className="slider" ref={ref}>
-            <Slider
-              isSliding={isSliding}
-              setSliding={setSliding}
-              thumbPos={thumbPos}
-              setThumbPos={setThumbPos}
-              sliderHeight={sliderHeight}
-              setSliderHeight={setSliderHeight}
+          <div className="slider">
+            <DurationSlider
+              onChange={handleDurationChange}
+              durationOfSession={durationOfSession}
             />
           </div>
-
-          {/* <div
-            className="buble-min"
-            style={{
-              top: `${thumbPos}px`,
-            }}
-          >
-            min
-          </div> */}
+          {console.log(durationOfSession)}
         </div>
         <div className="set-duration-button">
           <button
             onClick={() => {
               setTimeOpen(false);
-              setDuration(
-                thumbPos <= sliderHeight * 0.01
-                  ? positionToDuration(0, sliderHeight)
-                  : positionToDuration(
-                      thumbPos,
-                      sliderHeight - sliderHeight * 0.036
-                    )
-              );
+              setDuration(durdur);
               handleStop();
             }}
           >
             SET
           </button>
         </div>
-        {/* </div> */}
       </div>
     </div>
   );
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,141 +1,155 @@
-import React, { useRef, useEffect } from "react";
-import "./slider.css";
-// import {
-//   thumbTopPosMax,
-//   thumbTopPosMin,
-//   countVh,
-// } from "../../utils/DurationAndPosition.js";
-import { debounce } from "../../utils/Debounce";
-import { positionToDuration } from "../../utils/DurationAndPosition.js";
+import React from "react";
+import "./slider1.css";
+import Slider from "rc-slider";
 
-export const Slider = ({
-  isSliding,
-  setSliding,
-  setThumbPos,
-  thumbPos,
-  sliderHeight,
-  setSliderHeight,
-}) => {
-  // const sliderTrackRef = useRef(0);
-  const sliderTrackRef = useRef(0);
-  let trackHeight = 0;
+const marks = {
+  1: "1",
+  30: "30",
+};
 
-  useEffect(() => {
-    // 👇️ (better) use a ref
-    setSliderHeight(sliderTrackRef.current.clientHeight);
-    trackHeight = sliderTrackRef.current.firstChild.clientHeight;
-    console.log(trackHeight);
-    console.log(sliderHeight);
-  }, [sliderTrackRef.current.clientHeight]);
-  const handleClick = (e) => {
-    // console.log(
-    //   e,
-    //   e.clientY,
-    //   sliderTrackRef.current.offsetTop +
-    //     sliderTrackRef.current.parentElement.offsetTop,
-    //   sliderTrackRef.current.firstChild.clientHeight
-    // );
-    const position =
-      e.clientY -
-      (sliderTrackRef.current.offsetTop +
-        sliderTrackRef.current.parentElement.offsetTop) -
-      sliderTrackRef.current.firstChild.clientHeight / 2;
-    debounce(
-      position <= 0
-        ? setThumbPos(-sliderTrackRef.current.firstChild.clientHeight / 2)
-        : position >= sliderTrackRef.current.clientHeight
-        ? setThumbPos(
-            sliderTrackRef.current.clientHeight -
-              sliderTrackRef.current.firstChild.clientHeight
-          )
-        : setThumbPos(position),
-      50
-    );
-  };
-  console.log(thumbPos);
-  // const clientYref = useRef(0);
-
-  // const handleClick = (e) => {
-  //   console.log(e);
-  //   e.clientY - countVh * 35.8 < countVh * 3.308
-  //     ? (clientYref.current = countVh * thumbTopPosMin)
-  //     : e.clientY - countVh * 35.8 > countVh * thumbTopPosMax
-  //     ? (clientYref.current = countVh * thumbTopPosMax)
-  //     : (clientYref.current = e.clientY - countVh * 35.8);
-  //   debounce(setThumbPos(clientYref.current), 100);
-  // };
-  const handleMouseDown = () => {
-    setSliding(true);
-  };
-
-  const handleMouseUp = () => {
-    if (isSliding) {
-      setSliding(false);
-    }
-  };
-
-  const handleMouseMove = (e) => {
-    if (isSliding) {
-      e.preventDefault();
-      handleClick(e);
-    }
-  };
-
-  const handleTouchMove = (e) => {
-    const touch = e.targetTouches[0];
-    handleClick(touch);
-  };
+export const DurationSlider = ({ onChange }) => {
   return (
-    <div
-      className="slider-wrapper"
-      onMouseMove={handleMouseMove}
-      onTouchMove={handleTouchMove}
-      onMouseUp={handleMouseUp}
-    >
-      {" "}
-      <div
-        className="buble-number"
-        // style={{
-        //   top: "50%",
-        // }}
-      >
-        {thumbPos <= sliderHeight * 0.01
-          ? positionToDuration(0, sliderHeight)
-          : positionToDuration(thumbPos, sliderHeight - sliderHeight * 0.036)}
-        {` min`}
-      </div>
-      <div
-        className="slider-track"
-        onMouseMove={handleMouseMove}
-        onTouchMove={handleTouchMove}
-        onMouseUp={handleMouseUp}
-        onClick={handleClick}
-        ref={sliderTrackRef}
-        style={{
-          backgroundImage: `linear-gradient(to top, hsla(206, 39%, 30%, 1),  hsla(206, 39%, 30%, 1) ${Math.floor(
-            (1 - thumbPos / sliderTrackRef.current.clientHeight) * 100
-          )}%,hsla(204, 12%, 16%, 1) ${Math.floor(
-            (1 - thumbPos / sliderTrackRef.current.clientHeight) * 100
-          )}%, hsla(204, 12%, 16%, 1) ${Math.ceil(
-            (1 - thumbPos / sliderTrackRef.current.clientHeight) * 100
-          )}%)`,
-        }}
-      >
-        <div
-          className="slider-thumb"
-          onMouseDown={handleMouseDown}
-          onMouseUp={handleMouseUp}
-          style={{ top: `${thumbPos}px` }}
-        ></div>
-      </div>
-      {/* <div
-        className="buble-min"
-        style={{
-          top: `${thumbPos}px`,
-        }}
-      >
-        min
-      </div> */}
-    </div>
+    // <div className="slider-wrapper">
+    <Slider
+      vertical
+      min={1}
+      max={30}
+      defaultValue={3}
+      onChange={onChange}
+      marks={marks}
+    />
+    // </div>
   );
 };
+
+// export const Slider = ({
+//   isSliding,
+//   setSliding,
+//   setThumbPos,
+//   thumbPos,
+//   sliderHeight,
+//   setSliderHeight,
+// }) => {
+//   // const sliderTrackRef = useRef(0);
+//   const sliderTrackRef = useRef(0);
+//   let trackHeight = 0;
+
+//   useEffect(() => {
+//     // 👇️ (better) use a ref
+//     setSliderHeight(sliderTrackRef.current.clientHeight);
+//     trackHeight = sliderTrackRef.current.firstChild.clientHeight;
+//     console.log(trackHeight);
+//     console.log(sliderHeight);
+//   }, [sliderTrackRef.current.clientHeight]);
+//   const handleClick = (e) => {
+//     // console.log(
+//     //   e,
+//     //   e.clientY,
+//     //   sliderTrackRef.current.offsetTop +
+//     //     sliderTrackRef.current.parentElement.offsetTop,
+//     //   sliderTrackRef.current.firstChild.clientHeight
+//     // );
+//     const position =
+//       e.clientY -
+//       (sliderTrackRef.current.offsetTop +
+//         sliderTrackRef.current.parentElement.offsetTop) -
+//       sliderTrackRef.current.firstChild.clientHeight / 2;
+//     debounce(
+//       position <= 0
+//         ? setThumbPos(-sliderTrackRef.current.firstChild.clientHeight / 2)
+//         : position >= sliderTrackRef.current.clientHeight
+//         ? setThumbPos(
+//             sliderTrackRef.current.clientHeight -
+//               sliderTrackRef.current.firstChild.clientHeight
+//           )
+//         : setThumbPos(position),
+//       50
+//     );
+//   };
+//   console.log(thumbPos);
+//   // const clientYref = useRef(0);
+
+//   // const handleClick = (e) => {
+//   //   console.log(e);
+//   //   e.clientY - countVh * 35.8 < countVh * 3.308
+//   //     ? (clientYref.current = countVh * thumbTopPosMin)
+//   //     : e.clientY - countVh * 35.8 > countVh * thumbTopPosMax
+//   //     ? (clientYref.current = countVh * thumbTopPosMax)
+//   //     : (clientYref.current = e.clientY - countVh * 35.8);
+//   //   debounce(setThumbPos(clientYref.current), 100);
+//   // };
+//   const handleMouseDown = () => {
+//     setSliding(true);
+//   };
+
+//   const handleMouseUp = () => {
+//     if (isSliding) {
+//       setSliding(false);
+//     }
+//   };
+
+//   const handleMouseMove = (e) => {
+//     if (isSliding) {
+//       e.preventDefault();
+//       handleClick(e);
+//     }
+//   };
+
+//   const handleTouchMove = (e) => {
+//     const touch = e.targetTouches[0];
+//     handleClick(touch);
+//   };
+//   return (
+//     <div
+//       className="slider-wrapper"
+//       onMouseMove={handleMouseMove}
+//       onTouchMove={handleTouchMove}
+//       onMouseUp={handleMouseUp}
+//     >
+//       {" "}
+//       <div
+//         className="buble-number"
+//         // style={{
+//         //   top: "50%",
+//         // }}
+//       >
+//         {thumbPos <= sliderHeight * 0.01
+//           ? positionToDuration(0, sliderHeight)
+//           : positionToDuration(thumbPos, sliderHeight - sliderHeight * 0.036)}
+//         {` min`}
+//       </div>
+//       <div
+//         className="slider-track"
+//         onMouseMove={handleMouseMove}
+//         onTouchMove={handleTouchMove}
+//         onMouseUp={handleMouseUp}
+//         onClick={handleClick}
+//         ref={sliderTrackRef}
+//         style={{
+//           backgroundImage: `linear-gradient(to top, hsla(206, 39%, 30%, 1),  hsla(206, 39%, 30%, 1) ${Math.floor(
+//             (1 - thumbPos / sliderTrackRef.current.clientHeight) * 100
+//           )}%,hsla(204, 12%, 16%, 1) ${Math.floor(
+//             (1 - thumbPos / sliderTrackRef.current.clientHeight) * 100
+//           )}%, hsla(204, 12%, 16%, 1) ${Math.ceil(
+//             (1 - thumbPos / sliderTrackRef.current.clientHeight) * 100
+//           )}%)`,
+//         }}
+//       >
+//         <div
+//           className="slider-thumb"
+//           onMouseDown={handleMouseDown}
+//           onMouseUp={handleMouseUp}
+//           style={{ top: `${thumbPos}px` }}
+//         ></div>
+//       </div>
+//       {/* <div
+//         className="buble-min"
+//         style={{
+//           top: `${thumbPos}px`,
+//         }}
+//       >
+//         min
+//       </div> */}
+//     </div>
+//   );
+// };
