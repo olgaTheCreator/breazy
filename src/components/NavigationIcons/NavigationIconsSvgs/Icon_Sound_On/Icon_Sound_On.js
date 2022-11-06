068d8e65eabf9bf569c4098d1c403a57a06e6fb4
ojb <curylo.olga@gmail.com>
2022-11-06 19:53:36 +0000

    moving inline svgs to separate components

---
+++ src/components/NavigationIcons/NavigationIconsSvgs/Icon_Sound_On/Icon_Sound_On.js
@@ -0,0 +1,144 @@
+import React from "react";
+import "../navigationIconsSvgs.css";
+
+export const Icon_Sound_On = () => {
+  return (
+    <div className="nav_icon">
+      <svg
+        width="100%"
+        height="100%"
+        viewBox="0 0 54 54"
+        fill="none"
+        version="1.1"
+        id="svg886"
+        // sodipodi:docname="sound_on.svg"
+        // inkscape:version="1.2.1 (1:1.2.1+202210291244+9c6d41e410)"
+        // xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+        // xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+        xmlns="http://www.w3.org/2000/svg"
+        // xmlns:svg="http://www.w3.org/2000/svg"
+      >
+        {/* <sodipodi:namedview
+              id="namedview888"
+              pagecolor="#ffffff"
+              bordercolor="#666666"
+              borderopacity="1.0"
+              inkscape:showpageshadow="2"
+              inkscape:pageopacity="0.0"
+              inkscape:pagecheckerboard="0"
+              inkscape:deskcolor="#d1d1d1"
+              showgrid="false"
+              inkscape:zoom="17.706897"
+              inkscape:cx="26.995131"
+              inkscape:cy="28.971762"
+              inkscape:window-width="504"
+              inkscape:window-height="1404"
+              inkscape:window-x="1026"
+              inkscape:window-y="4"
+              inkscape:window-maximized="1"
+              inkscape:current-layer="svg886"
+            /> */}
+        <circle
+          cx="27"
+          cy="27"
+          r="27"
+          fill="#2f373c"
+          className="circleOuter"
+          id="circle848"
+        />
+        <g filter="url(#filter0_d_936_910)" id="g852">
+          <circle
+            cx="27"
+            cy="27"
+            r="23"
+            fill="url(#paint0_linear_936_910)"
+            shapeRendering="crispEdges"
+            id="circle850"
+            style={{ fill: "url(#paint0_linear_936_910)" }}
+          />
+        </g>
+        <path
+          d="m 15,30.3291 v -6.659 c 0,-0.6458 0.5824,-1.17 1.3,-1.17 h 4.6618 c 0.172,-6e-4 0.3422,-0.0312 0.5001,-0.0902 0.158,-0.059 0.3005,-0.145 0.419,-0.253 l 3.9,-3.8115 C 26.5999,17.6074 28,18.1305 28,19.1734 v 15.6535 c 0,1.0507 -1.417,1.5694 -2.2308,0.8167 l -3.887,-3.7902 c -0.1188,-0.111 -0.2628,-0.1996 -0.4229,-0.2604 -0.1602,-0.0608 -0.333,-0.0924 -0.5079,-0.0928 H 16.3 c -0.7176,0 -1.3,-0.5243 -1.3,-1.1711 z"
+          stroke="#8c9cb0"
+          strokeWidth="2.44874"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          id="path854"
+        />
+        <path
+          d="m 33,22 c 2.6667,2.286 2.6667,6.714 0,9"
+          stroke="#8c9cb0"
+          strokeWidth="2.44874"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          id="path856"
+        />
+        <path
+          d="m 35,19 c 5.3173,4.352 5.3493,11.6766 0,16"
+          stroke="#8c9cb0"
+          strokeWidth="2.44874"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          id="path858"
+        />
+        <defs id="defs884">
+          <filter
+            id="filter0_d_936_910"
+            x="0"
+            y="4"
+            width="54"
+            height="54"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood
+              floodOpacity="0"
+              result="BackgroundImageFix"
+              id="feFlood860"
+            />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+              id="feColorMatrix862"
+            />
+            <feOffset dy="4" id="feOffset864" />
+            <feGaussianBlur stdDeviation="2" id="feGaussianBlur866" />
+            <feComposite in2="hardAlpha" operator="out" id="feComposite868" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+              id="feColorMatrix870"
+            />
+            <feBlend
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_936_910"
+              id="feBlend872"
+            />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow_936_910"
+              result="shape"
+              id="feBlend874"
+            />
+          </filter>
+          <linearGradient
+            id="paint0_linear_936_910"
+            x1="27"
+            y1="4"
+            x2="27"
+            y2="50"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#292D30" id="stop877" />
+            <stop offset="0.961458" stopColor="#313538" id="stop879" />
+            <stop offset="1" stopColor="#292D30" stopOpacity="0" id="stop881" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+  );
+};
