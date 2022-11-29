33487b8e843d5b4844c61f553abbaa47ccbeaa78
ojb <curylo.olga@gmail.com>
2022-11-29 14:39:08 +0000

    beginSessionText tested

--- src/components/BeginSessionText/BeginSessionText.test.js
+++ src/components/BeginSessionText/BeginSessionText.test.js
@@ -33,3 +33,34 @@ test("Begin session text contains technique name", () => {
 
   expect(beginSessionText).toHaveTextContent(`${chosenTechnique.name}`);
 });
+
+test("If duration of session is longer than 1, minutes are in plural (contain 's')  ", () => {
+  const durationOfSession = 4;
+  const chosenTechnique = { name: "calm" };
+  const container = render(
+    <BeginSessionText
+      durationOfSession={durationOfSession}
+      chosenTechnique={chosenTechnique}
+    />
+  );
+
+  const beginSessionText = container.getByText(/begin your/i);
+
+  expect(beginSessionText).toHaveTextContent("minutes", { exact: true });
+});
+
+test("If duration of session 1, minutes are in singular (doesn't contain 's')  ", () => {
+  const durationOfSession = 1;
+  const chosenTechnique = { name: "calm" };
+  const container = render(
+    <BeginSessionText
+      durationOfSession={durationOfSession}
+      chosenTechnique={chosenTechnique}
+    />
+  );
+
+  const beginSessionText = container.getByText(/begin your/i);
+
+  expect(beginSessionText).not.toHaveTextContent("minutes", { exact: true });
+  expect(beginSessionText).toHaveTextContent("minute", { exact: true });
+});
